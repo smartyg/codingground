6219d7178f92e764c100027cdd20bbb81dc76f16
Martijn Goedhart <goedhart.martijn@gmail.com>
2016-12-02 13:21:26 +0100

    Created by Coding Ground Client

---
+++ New Project-20161202/main.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
+<?php
+require("AttrObject.php");
+require("TagObject.php");
+   echo "<h1>Hello, PHP!</h1>";
+?>
+</body>
+</html>
